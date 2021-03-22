e0370586a2ad6f59efe23047c6499da850927a48
alaa281997 <alaabbas97@gmail.com>
2021-03-22 15:44:36 +0200

    lap 6

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var userName 
 userName= prompt("whats your name ?");
-document.write('<h3> Hello '+userName+'</h3>');
 console.log(userName);
 
 
@@ -14,17 +13,21 @@ else {
 
 var age;
 age = prompt("whats your age");
-console.log(age);
-if (age >= 18 ) {
+var ageFunction = function(age){
+  console.log(age);
+  if (age >= 18 ) {
     alert('welcome to the site');
-} else if (age < 18) {
-    alert('sorry your not allowed');
-    while (age <= 17){
+  }else if (age < 18) {
+     alert('sorry your not allowed');
+     while (age <= 17){
      age = prompt("whats your age again");
     }
-} else {
+ }else {
     alert('please input a number');
+   }
+ 
 }
+ ageFunction(age);
 
 var x,y,result;
 
@@ -36,32 +39,37 @@ function myFunction(){
   console.log(result);
 
 }
-
-
 var carType= prompt("what type of car you want ?");
-while(carType !=='Mercedes' && carType !=='Ferrari' && carType !== 'Ford')
-   {
-     carType = prompt('please insert car name');
-   }
 
-var carNum = prompt('how many cars you want');
-
-var pic ='';
-var result='';
-
-if (carType=='Mercedes'){
-  pic=('<div style="display: inline-block;"> <img src="https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1534&q=80" alt="alternatetext" width="250 px">') ;
-  }else if (carType=='Ferrari'){
-  pic=('<div style="display: inline-block; padding: 50px;" ><img src="https://images.unsplash.com/photo-1511919884226-fd3cad34687c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="alternatetext" width="250 px">') ;
-  }else if (carType == 'Ford'){
-  pic=('<div style="display: inline-block;"><img src="https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="alternatetext" width="250 px">');
-  }
-  console.log(pic);
-  for (var j=0;j < carNum;j++){
-  result += pic;}
- console.log(result);
- 
+function getCarFunction(){
   
+  while(carType !=='Mercedes' && carType !=='Ferrari' && carType !== 'Ford')
+     {
+       carType = prompt('please insert car name');
+     }
+  
+  var carNum = prompt('how many cars you want');
+  
+  var pic ='';
+  var result='';
+  
+  if (carType=='Mercedes'){
+    pic=('<div style="display: inline-block;"> <img src="https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1534&q=80" alt="alternatetext" width="250 px">') ;
+    }else if (carType=='Ferrari'){
+    pic=('<div style="display: inline-block; padding: 50px;" ><img src="https://images.unsplash.com/photo-1511919884226-fd3cad34687c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="alternatetext" width="250 px">') ;
+    }else if (carType == 'Ford'){
+    pic=('<div style="display: inline-block;"><img src="https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="alternatetext" width="250 px">');
+    }
+    console.log(pic);
+    for (var j=0;j < carNum;j++){
+    result += pic;
+  }
+  return result
+ }
+            
+
+            
+
 
 
 
